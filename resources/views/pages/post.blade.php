48f7a88ac6114e76392fb1c638b55f697a671bed
rdwanmuhamad <mrdwn624@gmail.com>
2023-04-10 10:56:19 +0800

    call user name

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -5,14 +5,16 @@
 @endsection
 
 @section('content')
-    <div class="container mt-4">
+    <div class="container mb-5 border-bottom">
         @foreach ($posts as $post)
             <article class="mb-5">
-                <a href="{{ route('posts', $post->slug) }}">
+                <a href="{{ route('posts', $post->slug) }}" class="text-decoration-none">
                     <h2>{{ $post->title }}</h2>
                 </a>
-                <p>By Muhamad Ridwan in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+                <p>By {{ $post->user->name }} <a href="/category/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
                 <p>{{ $post->excerpt }}</p>
+
+                <p><a href="/category/{{ $post->category->slug }}" class="text-decoration-none">Read more...</a></p>
             </article>
         @endforeach
     </div>
